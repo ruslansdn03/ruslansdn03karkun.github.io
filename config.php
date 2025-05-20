2c53385895c1e655de71de59ce09dc6b71e60a82
ruslansdn03 <ahmadruslan711@gmail.com>
2025-05-20 09:45:41 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$host     = "localhost";    // Nama host
-$username = "root";         // Username database
-$password = "";   // Password database
+$host     = "https://ruslansdn03karkun.github.io/";    // Nama host
+$username = "Ruslan";         // Username database
+$password = "Ruslan";   // Password database
 $database = "ams_native";   // Nama database
